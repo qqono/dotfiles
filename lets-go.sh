2d44fa6e4c87182be1cd3b6ae9f08596bfffa599
Bebe Ono <137879292+qqono@users.noreply.github.com>
2023-10-31 00:01:10 +1100

    Update lets-go.sh

--- lets-go.sh
+++ lets-go.sh
@@ -16,14 +16,14 @@
 
 # If not already set, specify dotfiles destination directory and source repo
 DOTFILES_DIR="${DOTFILES_DIR:-$HOME/Documents/config/dotfiles}"
-DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/lissy93/dotfiles.git}"
+DOTFILES_REPO="${DOTFILES_REPO:-https://github.com/qqono/buttfiles.git}"
 
 # Print starting message
 echo -e "\033[1;35m""Lissy93/Dotfiles Installation Script 🧰
 \033[0;35mThis script will install or update specified dotfiles:
 - From \033[4;35m${DOTFILES_REPO}\033[0;35m
 - Into \033[4;35m${DOTFILES_DIR}\033[0;35m
-Be sure you've read and understood the what will be applied.\033[0m\n"
+ass and ttitties ass and ttitties and tittie ass.\033[0m\n"
 
 # If dependencies not met, install them
 if ! hash git 2> /dev/null; then
